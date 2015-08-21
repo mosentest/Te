323728b3811103bf0b12461539439f651b6b54a5
moziqi <709847739@qq.com>
2015-08-21 16:45:51 +0800

    添加一个开源UI

--- app/src/main/java/org/moziqi/fragment/BlankHomeFragment.java
+++ app/src/main/java/org/moziqi/fragment/BlankHomeFragment.java
@@ -1,19 +1,22 @@
 package org.moziqi.fragment;
 
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
 import moziqi.te.R;
 
 /**
  * A simple {@link Fragment} subclass.
- *
  */
 public class BlankHomeFragment extends Fragment {
 
@@ -21,6 +24,7 @@ public class BlankHomeFragment extends Fragment {
     public BlankHomeFragment() {
         // Required empty public constructor
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,26 +34,26 @@ public class BlankHomeFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        Log.e("BlankHomeFragment","onResume");
+        Log.e("BlankHomeFragment", "onResume");
     }
 
 
     @Override
     public void onPause() {
         super.onPause();
-        Log.e("BlankHomeFragment","onPause");
+        Log.e("BlankHomeFragment", "onPause");
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        Log.e("BlankHomeFragment","onStop");
+        Log.e("BlankHomeFragment", "onStop");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.e("BlankHomeFragment","onDestroy");
+        Log.e("BlankHomeFragment", "onDestroy");
     }
 
 
@@ -57,7 +61,18 @@ public class BlankHomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_blank_home, container, false);
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getActivity().getSupportFragmentManager(), FragmentPagerItems.with(getActivity())
+                .add(R.string.more, VMoreFragment.class)
+                .add(R.string.msg, VMsgFragment.class)
+                .create()
+        );
+        ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+        SmartTabLayout viewPagerTab = (SmartTabLayout) rootView.findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
+        return rootView;
     }
 
 
--- app/src/main/java/org/moziqi/fragment/VMsgFragment.java
+++ app/src/main/java/org/moziqi/fragment/VMsgFragment.java
@@ -1,9 +1,9 @@
 package org.moziqi.fragment;
 
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +12,6 @@ import moziqi.te.R;
 
 /**
  * A simple {@link Fragment} subclass.
- *
  */
 public class VMsgFragment extends GeneralFragment {
 
@@ -26,6 +25,7 @@ public class VMsgFragment extends GeneralFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setTitle(getResources().getString(R.string.msg));
+        Log.e(VMsgFragment.class.getName(), "onCreate");
     }
 
     @Override
@@ -35,5 +35,22 @@ public class VMsgFragment extends GeneralFragment {
         return inflater.inflate(R.layout.fragment_vmsg, container, false);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.e(VMsgFragment.class.getName(), "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.e(VMsgFragment.class.getName(), "onPause");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.e(VMsgFragment.class.getName(), "onDestroy");
+    }
 
 }
